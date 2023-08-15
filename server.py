2d7583eacc1af55e6689a87ce43ab39a63baa41c
Siva50005 <sivanayya11@gmail.com>
2023-08-15 22:36:31 +0530

    Added the add and subtract function definition

--- server.py
+++ server.py
@@ -1,19 +1,32 @@
 from flask import Flask
+from requests import request
+from flask import jsonify
+
 
 app = Flask(__name__)
 
 
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    return ''
+    return 'Hello World!'
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
-    return ''
+    data = request.get_json()  # Get JSON data from the request
+    if 'first' in data and 'second' in data:
+        result = data['first'] + data['second']
+        return jsonify({"result": result})
+    else:
+        return jsonify({"error": "Invalid input format"}), 400
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
-    return ''
+    data = request.get_json()  # Get JSON data from the request
+    if 'first' in data and 'second' in data:
+        result = data['first'] - data['second']
+        return jsonify({"result": result})
+    else:
+        return jsonify({"error": "Invalid input format"}), 400
 
 if __name__ == '__main__':
     app.run(port=8080,host='0.0.0.0')
